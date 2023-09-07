bacc2b8ba9adcd771b65c9da3e1132761de470b2
hodograph <jjdeanman@gmail.com>
2023-09-07 16:15:28 -0400

    Added stats

--- Shared/Transaction.cs
+++ Shared/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BlazorApp.Shared
@@ -14,8 +15,31 @@ namespace BlazorApp.Shared
 
         public double Total { get; set; }
 
-        public TransactionType TransactionType { get; set; }
-
         public List<Debt> Debts { get; set; } = new List<Debt>();
+
+        public List<Debt> CalculatePayments()
+        {
+            List<Debt> debts = new List<Debt>();
+
+            Debts.ForEach(x => debts.Add(new Debt() { Amount = x.Amount, Debtor = x.Debtor }));
+
+            double debtsTotal = 0;
+            debts.ForEach(x => debtsTotal += x.Amount);
+            double remainder = Total - debtsTotal;
+
+            if (!debts.Any(x => x.Debtor == Payer))
+            {
+                debts.Add(new Debt()
+                {
+                    Debtor = Payer,
+                    Amount = 0
+                });
+            }
+
+            double remainingSplit = remainder / debts.Count;
+            debts.ForEach(x => x.Amount += remainingSplit);
+
+            return debts;
+        }
     }
 }
--- Shared/User.cs
+++ Shared/User.cs
@@ -6,8 +6,24 @@ namespace BlazorApp.Shared
 {
     public class User
     {
-        public string email { get; set; }
+        public string email { get; set; } = string.Empty;
 
-        public string DisplayName { get; set; }
+        public string DisplayName { get; set; } = string.Empty;
+
+        public override bool Equals(object obj)
+        {
+            bool equal = false;
+            if (obj is User user)
+            {
+                equal = user.email == email;
+            }
+
+            return equal;
+        }
+
+        public override int GetHashCode()
+        {
+            return email.GetHashCode();
+        }
     }
 }
